a97a3cce4c06b167e5c401f2994edcffd34115d1
jrnibs <johnnibato13@gmail.com>
2026-02-10 20:05:15 +0800

    lesson php

---
+++ PHP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcone</title>
+</head>
+<body>
+
+    <?php include 'navbar.php'; ?>
+    <?php echo $header; ?>
+
+    <?php echo "<h1>WELCOME TO MY PAGE</h1>"; ?>
+    <h3><?php echo $full_Name; ?></h3> 
+    <h3><?php echo $course . " | " . $subject ; ?></h3> 
+</body>
+</html>
+
---
+++ PHP/navbar.php
@@ -0,0 +1,16 @@
+<?php 
+$full_Name = "JOHN ROOSEVELT NIBATO";
+$course = "CONSCI";
+$subject = 222;
+
+?>
+<?php $header = "
+<header>
+    <nav>
+        <li><a href='index.php'>HOMES | <?php echo $full_Name; ?></a></li>
+        <li><a href='about.php'>ABOUT</a></li>
+        <li><a href=''>ACTIVITY</a></li>
+    </nav>
+</header> ";
+
+?>
