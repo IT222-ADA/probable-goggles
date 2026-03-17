a110c1b6713e79222bbd8356a4ea3235ec25112a
jrnibs <johnnibato13@gmail.com>
2026-03-17 17:25:33 +0800

    Add input validation for calculator operations in calculate.php

--- OOP-CALCULATOR/calculate.php
+++ OOP-CALCULATOR/calculate.php
@@ -5,7 +5,11 @@ $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $operation = $_POST['operation'];
 
-$calculator = new CalcClass($num1, $num2, $operation);
+if(isset($num1) && isset($num2) && isset($operation)){
+    $calculator = new CalcClass($num1, $num2, $operation);
+    $calculator->displayResult();
+} else {
+    echo "Please provide all inputs.";
+}
 
-$calculator->displayResult();
 
